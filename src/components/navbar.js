822f93bd2aef2a2acd899c286dbde34ce8ae7a0c
Austin Hillyard - Mars <haust16@gmail.com>
2021-07-09 00:56:03 -0600

    fixed navbar disappearing -- sort of. The button for the dropdown shows up now, but it doesn't do anything

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,9 +25,9 @@ function Navbar(props) {
             <img src={logo} height="25" alt="Pawtomatic Logo"/>
             Pawtomatic
           </Link>
-          {/*}<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-          </button>*/}
+          </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
@@ -57,12 +57,6 @@ function Navbar(props) {
               </li>
               */}
             </ul>
-            {/*
-            <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            */}
           </div>
         </nav>
       </div>
