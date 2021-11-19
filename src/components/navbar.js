dc56fc0b0ffd364660d0a4168520b85806dbcee0
Austin Hillyard <austin.hillyard@sdl.usu.edu>
2021-11-18 17:31:06 -0700

    PAW-14 #time 15m #comment Refreshing myself on how to run Node, build, and deploy the site.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,11 +25,13 @@ function Navbar(props) {
             <img src={logo} height="25" alt="Pawtomatic Logo!"/>
             Pawtomatic
           </Link>
+          {/*
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
+          */}
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className={`nav-item ${isProject ? "active" : ""}`}>
                 <Link className="nav-link" to="/project">Project</Link>
